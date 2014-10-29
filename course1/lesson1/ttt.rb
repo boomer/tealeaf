f489ef5b6cb8c743aaf170dd348984974b72f23a
Michael Boomershine <boomer@shorttakes.com>
2014-10-29 15:01:41 -0700

    Refactored everything based on the solution...

--- course1/lesson1/ttt.rb
+++ course1/lesson1/ttt.rb
@@ -1,6 +1,6 @@
 # ttt.rb
 # Michael Boomershine
-# Edited 10/17/14
+# Edited 10/29/14
 
 # Problem
 # draw a grid of 11 rows by 17 columns
@@ -11,77 +11,85 @@
 # update that number from the array 
 # randomly generate a pick for the computer and replace that number with an ""
 # refresh the grid with the correct value in the space
-# loop until all the places are filled - or there is a winning combination
+# loop until all the places are filled or there is a winner
+# 
 
 require 'pry'
 
-PLAYER_CHOICES = []
-COMPUTER_CHOICES = []
-AVAILABLE_CHOICES = [1,2,3,4,5,6,7,8,9]
-WINS = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
-POSITIONS = { 1 => " ", 2 => " ", 3 => " ", 4 => " ", 5 => " ", 6 => " ", 7 => " ", 8 => " ", 9 => " "}
+def create_positions
+  p = {}
+  (1..9).each {|position| p[position] = ' '}
+  p
+end
 
-def render_grid
-  #clear the screen
+def render_grid(n)
   puts "\e[H\e[2J"
-  #show the grid
   puts "     |     |     "
-  puts "  #{POSITIONS.fetch(1)}  |  #{POSITIONS.fetch(2)}  |  #{POSITIONS.fetch(3)}  "
-  puts "     |     |     "
-  puts "-----+-----+-----"
-  puts "     |     |     "
-  puts "  #{POSITIONS.fetch(4)}  |  #{POSITIONS.fetch(5)}  |  #{POSITIONS.fetch(6)}  "
+  puts "  #{n[1]}  |  #{n[2]}  |  #{n[3]}  "
   puts "     |     |     "
   puts "-----+-----+-----"
   puts "     |     |     "
-  puts "  #{POSITIONS.fetch(7)}  |  #{POSITIONS.fetch(8)}  |  #{POSITIONS.fetch(9)}  "
+  puts "  #{n[4]}  |  #{n[5]}  |  #{n[6]}  "
+  puts "     |     |     "
+  puts "-----+-----+-----"  
+  puts "     |     |     "
+  puts "  #{n[7]}  |  #{n[8]}  |  #{n[9]}  "
   puts "     |     |     "
 end
 
-def play_game
-  render_grid
+def player_picks(n)
   puts "                 "
   puts "Choose a position (from 1-9) to place a piece:"
-  p = gets.chomp.to_i
-
-  if AVAILABLE_CHOICES.include?(p)
-
-    PLAYER_CHOICES.push(p)
-    AVAILABLE_CHOICES.delete(p)
-    POSITIONS.update({ p => "X"})  
-    render_grid
-
-  else
-
-    puts "                 "
-    puts "That position is taken. Choose a different position (from 1-9) to place a piece:"
-    p = gets.chomp.to_i 
-    PLAYER_CHOICES.push(p)
-    AVAILABLE_CHOICES.delete(p)
-    POSITIONS.update({ p => "X"})  
-    render_grid
-
-  end
-  
-  sleep (1)
-
-  c = AVAILABLE_CHOICES.sample
-  COMPUTER_CHOICES.push(c)
-  AVAILABLE_CHOICES.delete(c)
-  POSITIONS.update({ c => "O"}) 
-  render_grid
-
-  #what happens if computer chooses a spot that's already been taken
+  player_choice = gets.chomp.to_i
+  check_position(n,player_choice)
 end
 
-until AVAILABLE_CHOICES.nil?
-  play_game
-  binding.pry
-  if WINS.include?(PLAYER_CHOICES.sort)
-     puts "YOU WIN!!" 
-  elsif WINS.include?(COMPUTER_CHOICES.sort)
-     puts "YOU LOSE!!" 
+def check_position(n,p)
+  if n[p] != ' '
+    puts "That position is taken."
+    player_picks(n)
   else
-     puts "It's a tie!"
+    n[p] = 'X'
   end
 end
+
+def computer_picks(n)
+  p = n.select { |k,v| v == ' ' }.keys.sample
+  n[p] = 'O'
+  sleep(0.5)
+end
+
+def available_positions(n)  
+  positions = n.select { |k,v| v == ' ' }.keys
+end
+
+def calculate_wins(n)
+  wins = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
+  wins.each do |win|
+    return "Player" if n[win[0]] == "X" && n[win[1]] == "X" &&  n[win[2]] == "X"
+    return "Computer" if n[win[0]] == "O" && n[win[1]] == "O" &&  n[win[2]] == "O"
+  end
+  nil
+end
+
+def announce_winner(winner)
+ puts "#{winner} won!"
+end
+
+grid = create_positions
+render_grid(grid)
+
+begin 
+  player_picks(grid)
+  render_grid(grid)
+  computer_picks(grid)
+  render_grid(grid)
+  winner = calculate_wins(grid)
+end until winner || available_positions(grid).empty?
+
+if winner
+  announce_winner(winner)
+else
+  puts "It's a tie."
+end
+
